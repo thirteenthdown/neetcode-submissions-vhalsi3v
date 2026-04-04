0882bf7c3c8a7b64a4aee8e7d0985d8688914811
Rahul Dutta <140334684+thirteenthdown@users.noreply.github.com>
2026-04-04 13:12:39 +0530

    Add: count-number-of-islands - submission-0

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-0.cpp
@@ -0,0 +1,29 @@
+class Solution {
+private:
+    void dfs(int i, int j, int n, int m, vector<vector<char>>& grid, vector<vector<bool>>& isVisited){
+        if(i<0 || j<0 || i>=n || j>=m || grid[i][j] != '1' || isVisited[i][j]){
+            return;
+        }
+        isVisited[i][j] = true;
+        dfs(i+1, j, n, m, grid, isVisited);
+        dfs(i-1, j, n, m, grid, isVisited);
+        dfs(i, j+1, n, m, grid, isVisited);
+        dfs(i, j-1, n, m, grid, isVisited);
+    }
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<bool>> isVisited(n, vector<bool>(m, false));
+        int islands = 0;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(grid[i][j] == '1' && !isVisited[i][j]){
+                    dfs(i, j, n, m, grid, isVisited);
+                    islands++;
+                }
+            }
+        }
+        return islands;
+    }
+};
